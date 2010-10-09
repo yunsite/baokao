f0dd69003247ef05aa6830543b0d38501baab673
zibin5257 <zibin5257@4115fda5-301c-d9c1-741e-7220542d9565>
2010-10-09 10:50:10 +0000

    增加    后台-报表统计，未合格报表功能

--- trunk/baokao/Lib/Action/Lfy/ReportAction.class.php
+++ trunk/baokao/Lib/Action/Lfy/ReportAction.class.php
@@ -77,5 +77,68 @@ class ReportAction extends Action{
         
         $this->display();
     }
+
+    /**
+     * 未合格名单报表
+     */
+
+    function nohege_report(){
+        $Stu_info=M('Stu_info');
+        //获取学生信息表中不重复的年级列表并赋值到模板
+        $this->assign('year_list', $Stu_info->field('distinct stu_join')->select());
+        //获取学生信息表中不重复的系别列表并赋值到模板
+        $this->assign('depart_list', $Stu_info->field('distinct depart_name')->select());
+
+        /**
+         * 进行处理获取到的查询条件数据
+         */
+        $year=addslashes($_GET['year']);
+        $depart=addslashes($_GET['depart']);
+
+        //如果年级和系别为空，则默认赋值为选择
+        if($year==''){
+            $year='select';
+        }
+        if($depart==''){
+            $depart='select';
+        }
+        //对选择全部年级的情况进行处理
+        if($year=='all'){
+            $year_sql='';
+        }else{
+            $year_sql=" and stu_join='{$year}'";
+        }
+        //对选择全部系别的时候进行处理
+        if($depart=='all'){
+            $depart_sql='';
+        }else{
+            $depart_sql=" and depart_name='{$depart}'";
+        }
+        //输出当前查询的年级和系别
+        $this->assign('year', $year);
+        $this->assign('depart', $depart);
+
+        //如果年级和系别有一个未选择，则不进行进一步的查询操作
+        if($year!='select' || $depart!='select'){
+            //使用原生sql语句查询排除合格表的记录
+            $sql = "SELECT stu_no,stu_name,stu_sex,depart_name,stu_class,"
+            ."stu_join,"
+            ."((select bk_num from lfy_bk_num where lfy_bk_num.stu_join=lfy_stu_info.stu_join)"
+            ."-(select count(stu_no) from lfy_stubk_temp where lfy_stubk_temp.stu_no=lfy_stu_info.stu_no)) num "
+            ."from lfy_stu_info "
+            ."where 1 and 1 {$year_sql}{$depart_sql} "
+            ."and NOT EXISTS (SELECT * FROM lfy_stu_hege WHERE lfy_stu_hege.stu_no = lfy_stu_info.stu_no) "
+            ."order by stu_join desc,depart_name,stu_class,stu_no desc";
+            //$list=$Stu_info->query($sql);
+            
+            $this->assign('stu_list', $Stu_info->query($sql));
+            //将当前的数量赋值到模板
+            $this->assign('stu_num', count($list));
+        }else{
+            $this->assign('stu_num', 0);
+        }
+        
+        $this->display();
+    }
 }
 ?>
